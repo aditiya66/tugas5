4537a05987408553e1e6b1f00d22e283ea1cabdb
Aditiya_Nugraha <aditiyanugraha56@gmail.com>
2015-10-09 12:52:26 +0700

    revisi5

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,188 +1,137 @@
 class ArticlesController < ApplicationController
-
   def index
 
-  	@articles=Article.all.order(:created_at).page(params[:page]).per(5)
+    @articles=Article.all.order(:created_at).page(params[:page]).per(5)
     @comments = Comment.all
-  
-  respond_to do |format|
+
+    respond_to do |format|
       format.html
       format.csv { send_data Article.to_csv }
-      format.xls 
+      format.xls
 
     end
-end
-
-
- 
-
-
-
+  end
 
   def show
     @article = Article.find_by_id(params[:id])
- @comments = @article.comments.order("id desc")   
-  @comment = Comment.new
-  respond_to do |format|
+    @comments = @article.comments.order("id desc")
+    @comment = Comment.new
+    respond_to do |format|
       format.html
       format.csv { send_data Article.to_csv }
       format.xls
     end
 
-
   end
 
-  
   def new
-  	@article_import = Article.new
-    
-  end
+    @article_import = Article.new
 
-  
+  end
 
   def edit
-  	@article = Article.find_by_id(params[:id])
+    @article = Article.find_by_id(params[:id])
   end
-
-  
-
 
   def create
 
+    @article_import = Article.new(params[:article_import])
 
-
-
-      
-      @article_import = Article.new(params[:article_import])
-
-
-      if @article_import.save
+    if @article_import.save
       redirect_to root_url, notice: "Imported products successfully."
     else
       render :new
     end
-  
-
-
-
-
-
-
-
-    #     flash[:error] = "data not valid"
-
-    #     render 'new'
-
-    # end
 
   end
 
-     def update
+  def update
 
     @article = Article.find_by_id(params[:id])
-    
+
     if @article.update(params_article)
-    
-    flash[:notice] = "Success Update Records"
-    
-    redirect_to action: 'index'
 
- else
+      flash[:notice] = "Success Update Records"
 
-    flash[:error] = "data not valid"
+      redirect_to action: 'index'
 
-    render 'edit'
+    else
 
- end
+      flash[:error] = "data not valid"
 
-end
+      render 'edit'
 
+    end
 
-def import
-  
-valid_keys= ["id","title","content","status"]
+  end
 
-total_row = 0
+  def import
+
+    valid_keys= ["id","title","content","created_at","updated_at"]
+
+    total_row = 0
     spreadsheet = Import.open_spreadsheet(params[:file])
 
-    # spreadsheet.sheets.each_with_index do |sheet, index|
-    #   spreadsheet.default_sheet = spreadsheet.sheets[index]
-# byebug
+    header = Array.new
+    spreadsheet.row(1).each { |row| header << row.downcase.tr(' ', '_') }
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+        valid=row.to_hash.slice(*valid_keys)
+
+      data = Article.create(valid)
+
+      spreadsheet.default_sheet= spreadsheet.sheets.last
+
       header = Array.new
       spreadsheet.row(1).each { |row| header << row.downcase.tr(' ', '_') }
       (2..spreadsheet.last_row).each do |i|
         row = Hash[[header, spreadsheet.row(i)].transpose]
-          data = Article.create(row)
-      
+        data=row.to_hash.slice(*valid_keys)
 
-spreadsheet.default_sheet= spreadsheet.sheets.last
+        comment = Article.last.comments.create(data)
 
- header = Array.new
-      spreadsheet.row(1).each { |row| header << row.downcase.tr(' ', '_') }
-      (2..spreadsheet.last_row).each do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-   data=row.to_hash.slice(*valid_keys)
+      end
+    end
 
-
-               comment = Article.last.comments.create(data)
-
-
-    @articles=Article.all.order(:created_at).page(params[:page]).per(5)
-    @comments = Comment.all
-
-end
-
-
-
-
-      # end
-
-end
-
-
-
-
-  redirect_to root_url, notice: "Products imported."
-end
-
- 
- def destroy
-        
-        @article = Article.find_by_id(params[:id])
-
-        if @article.destroy
-        
-        flash[:notice] = "Success Delete a Records"
-        
-        redirect_to action: 'index'
-        
-        else
-        
-        flash[:error] = "fails delete a records"
-        
-        redirect_to action: 'index'
-        
-        end
-
-end
-
-
-  private 
-  def params_article
-params.require(:article).permit(:title, :content, :status)
-
+    redirect_to root_url, notice: "Products imported."
   end
 
+  def destroy
 
+    @article = Article.find_by_id(params[:id])
+
+    if @article.destroy
+
+      flash[:notice] = "Success Delete a Records"
+
+      redirect_to action: 'index'
+
+    else
+
+      flash[:error] = "fails delete a records"
+
+      redirect_to action: 'index'
+
+    end
+
+  end
 
   private
-    def change_format
-      request.format = "xls"
-    end
-  def params_comment
-params.require(:comment).permit(:content)
+
+  def params_article
+    params.require(:article).permit(:title, :content, :status)
 
   end
 
+  private
+
+  def change_format
+    request.format = "xls"
+  end
+
+  def params_comment
+    params.require(:comment).permit(:content)
+
+  end
 
 end
--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,71 +1,52 @@
-
 require 'csv'
 require 'roo'
 require 'spreadsheet'
 require 'zip'
+
 # require 'zip/zip'
 
 require 'roo/base'
 
-
 class ImportsController < ApplicationController
+  def import
 
+    valid_keys= ["title","content","status"]
 
-
-
-def import
-  
-valid_keys= ["title","content","status"]
-
-total_row = 0
+    total_row = 0
     spreadsheet = Import.open_spreadsheet(params[:file])
 
     # spreadsheet.sheets.each_with_index do |sheet, index|
     #   spreadsheet.default_sheet = spreadsheet.sheets[index]
-# byebug
+    # byebug
+    header = Array.new
+    spreadsheet.row(1).each { |row| header << row.downcase.tr(' ', '_') }
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      data = Article.create(row)
+
+      spreadsheet.default_sheet= spreadsheet.sheets.last
+
       header = Array.new
       spreadsheet.row(1).each { |row| header << row.downcase.tr(' ', '_') }
       (2..spreadsheet.last_row).each do |i|
         row = Hash[[header, spreadsheet.row(i)].transpose]
-          data = Article.create(row)
-      
+        user_is=Article.all.select(:id)
+        # accesible=["id","content"]
 
-spreadsheet.default_sheet= spreadsheet.sheets.last
+        # comment= row.to_hash.slice(accesible)
+        # byebug
 
- header = Array.new
-      spreadsheet.row(1).each { |row| header << row.downcase.tr(' ', '_') }
-      (2..spreadsheet.last_row).each do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-          user_is=Article.all.select(:id)
-# accesible=["id","content"]
+        comment = Comment.create(row)
 
-    # comment= row.to_hash.slice(accesible)
-    # byebug
-
-               comment = Comment.create(row)
-
-
-  	@articles=Article.all.order(:created_at).page(params[:page]).per(5)
-    @comments = Comment.all
+        @articles=Article.all.order(:created_at).page(params[:page]).per(5)
+        @comments = Comment.all
         redirect_to action: 'articles/index'
 
+      end
 
-end
+    # end
 
-
-
-
-      # end
-
-end  
-end
-
-  
-
-
-
-
-
-
+    end
+  end
 
 end
